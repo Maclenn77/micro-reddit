db1ea8e4489bc8806d0cec75a8ba91b11c178c6f
Pedro Donda <phfdonda@gmail.com>
2020-06-03 16:04:28 -0300

    Add validation for username - not working yet

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,25 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
+end
+
+class AlphanumericValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /[0-9a-z]/ 
+      record.errors[attribute] << (options[:message] || "the name must be composed of only letters and numbers")
+    end
+  end
+end
+
 class User < ApplicationRecord
   has_many :comments, :dependent =>  :destroy
   has_many :posts, :dependent =>  :destroy
   has_many :subscriptions, :dependent =>  :destroy
   has_many :subreddits, through: :subscriptions
 
-  validates :username, :email, presence: true
-
+  validates :username, presence: true, alphanumeric: true
+  validates :email, presence: true, email: true
 end
